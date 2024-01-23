67d1f50b719a71959748cce092e080494a59cfa7
The-Estonian <Zaar2213@gmail.com>
2024-01-23 23:28:42 +0200

    23.01.24:23:30 update

--- frontend/backendConnection/connection.js
+++ frontend/backendConnection/connection.js
@@ -1,4 +1,12 @@
+import { NewElement } from '../helpers/elements.js';
+
+const root = document.querySelector('#root');
+
 export const SendLoginData = async (username, password) => {
+  const modal = NewElement('div', 'backdrop');
+  const spinner = NewElement('div', 'loader');
+  modal.appendChild(spinner);
+  root.appendChild(modal);
   return new Promise((resolve, reject) => {
     var xhr = new XMLHttpRequest();
     var url = 'http://localhost:8080/login';
@@ -9,7 +17,7 @@ export const SendLoginData = async (username, password) => {
       if (xhr.readyState === 4) {
         if (xhr.status === 200) {
           let json = JSON.parse(xhr.responseText);
-          // console.log(json.registration);
+          root.removeChild(modal);
           resolve(json);
         } else {
           reject(new Error('No response after login'));
